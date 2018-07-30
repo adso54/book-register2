48a71e0c44e0ef0e74eb2663c7a34b1a92e9f792
Kamil <kamil.danielski@gmail.com>
2018-07-30 19:07:51 +0200

    Initial commit

---
+++ src/main/java/pl/kamil_danielski/bookregister2/BookRegister2Application.java
@@ -0,0 +1,12 @@
+package pl.kamil_danielski.bookregister2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRegister2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRegister2Application.class, args);
+	}
+}
